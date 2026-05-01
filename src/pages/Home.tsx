e138c66917d9259b33b8bbca8bfb1a87b202545e
Patrick Petrushonis <pwp904@gmail.com>
2026-05-01 08:57:49 -0700

    Updated button and construction styles.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,14 @@ const Home = ({ data }) => {
     return (
     <>
       <div className="content flex-content flex--center">
-        <div className="section__container flex-content flex--center">
-          <div className="section__content section__content--construction flex__small--12 flex__large--12 center">
-            <h1 className="center" id="section-construction">Under Construction</h1>
-            <p>
-              Exciting updates coming soon!
-            </p>
+        <div className="section__container">
+          <div className="section__content section__content--construction flex-content flex--center">
+            <div className="flex__small--12 flex__large--12 center">
+              <h1 id="section-construction">Under Construction</h1>
+              <p>
+                Exciting updates coming soon!
+              </p>
+            </div>
           </div>
         </div>
       </div>
